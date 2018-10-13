9d917a1d7a47aff918719721ffd36f38eef001b9
kkyyhh96 <kkyyhh96@163.com>
2018-10-13 00:13:08 -0500

    20181012

---
+++ Array/Q31_next_permutation.py
@@ -0,0 +1,10 @@
+class Solution:
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+
+
+s = Solution()
+print(s.nextPermutation([1,2,3]))
